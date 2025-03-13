3e5ea212dbddb1fcda5e2e666437cb4e15fad31d
Fy <fy.me@outlook.com>
2025-03-13 18:20:14 +0800

    方法提取，自定义相机离屏等

--- Apps/FY/common/methods.js
+++ Apps/FY/common/methods.js
@@ -1,7 +1,8 @@
+/*eslint-disable*/
 const { Framebuffer, Texture, PixelFormat, PixelDatatype } = Cesium;
 
 // 创建一个FBO
-function createFrameBuffer(context) {
+const createFrameBuffer = (context) => {
   const width = context.canvas.clientWidth;
   const height = context.canvas.clientHeight;
 
@@ -22,4 +23,32 @@ function createFrameBuffer(context) {
       pixelDatatype: PixelDatatype.UNSIGNED_SHORT
     })
   });
+}
+
+// 将FBO的纹理渲染到Canvas中
+const renderFBOToCanvas = (viewer, fbo, canvas = document.getElementById("outputCanvas")) => {
+  const ctx = canvas.getContext("2d");
+
+  const { drawingBufferWidth, drawingBufferHeight } = viewer.scene.context;
+  const pixels = viewer.scene.context.readPixels({
+    x: 0,
+    y: 0,
+    width: drawingBufferWidth,
+    height: drawingBufferHeight,
+    framebuffer: fbo,
+  });
+  canvas.width = drawingBufferWidth;
+  canvas.height = drawingBufferHeight;
+
+  const imgData = new ImageData(
+    new Uint8ClampedArray(pixels),
+    drawingBufferWidth,
+    drawingBufferHeight
+  );
+  ctx.putImageData(imgData, 0, 0, 0, 0, drawingBufferWidth, drawingBufferHeight);
+  ctx.translate(0, drawingBufferHeight);
+  ctx.scale(1, -1);
+  ctx.drawImage(canvas, 0, 0);
+  canvas.style.height = (drawingBufferHeight * 0.3) + "px";
+  canvas.style.width = (drawingBufferWidth * 0.3) + "px";
 }
