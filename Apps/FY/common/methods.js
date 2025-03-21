acfff903ec6b4d7ff90810fd08fb20010f10f244
Fy <fy.me@outlook.com>
2025-03-21 11:28:22 +0800

    提取方法，补充注释

--- Apps/FY/common/methods.js
+++ Apps/FY/common/methods.js
@@ -52,3 +52,15 @@ const renderFBOToCanvas = (viewer, fbo, canvas = document.getElementById("output
   canvas.style.height = (drawingBufferHeight * 0.3) + "px";
   canvas.style.width = (drawingBufferWidth * 0.3) + "px";
 }
+
+const defaultMeshUrl = "http://114.242.26.126:7000/examples/examples/1.%20%E6%95%99%E7%A8%8B%E7%A4%BA%E4%BE%8B/1.%20%E5%9F%BA%E7%A1%80%E5%9C%BA%E6%99%AF%E5%AF%B9%E8%B1%A1%E7%A4%BA%E4%BE%8B/1.%203dtiles%E6%95%B0%E6%8D%AE%E5%8A%A0%E8%BD%BD%E7%A4%BA%E4%BE%8B20241118//revit_school_3dtiles1.1/tileset.json"
+// 加载3DTiles
+const addTileset = async (url = defaultMeshUrl, options = {}, isZoomTo = true) => {
+  const _tileset = await Cesium.Cesium3DTileset.fromUrl(
+    url,
+    options,
+  );
+  tileset = viewer.scene.primitives.add(_tileset);
+  viewer.zoomTo(tileset);
+  return tileset;
+};
